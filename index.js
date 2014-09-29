e51c9e0e97807f2e793cc74a2b83e126ba010867
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-09-29 09:34:06 +0200

    Adding node implementation from the original repo

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-1;
+/*jshint node:true */
+
+'use strict';
+
+var sequence = function (tasks, names, results, missing, recursive, nest) {
+  names.forEach(function (name) {
+    if (results.indexOf(name) !== -1) {
+      return; // de-dup results
+    }
+    var node = tasks[name];
+    if (!node) {
+      missing.push(name);
+    } else if (nest.indexOf(name) > -1) {
+      nest.push(name);
+      recursive.push(nest.slice(0));
+      nest.pop(name);
+    } else if (node.dep.length) {
+      nest.push(name);
+      sequence(tasks, node.dep, results, missing, recursive, nest); // recurse
+      nest.pop(name);
+    }
+    results.push(name);
+  });
+};
+
+// tasks: object with keys as task names
+// names: array of task names
+module.exports = function (tasks, names) {
+  var results = []; // the final sequence
+  var missing = []; // missing tasks
+  var recursive = []; // recursive task dependencies
+
+  sequence(tasks, names, results, missing, recursive, []);
+
+  if (missing.length || recursive.length) {
+    results = []; // results are incomplete at best, completely wrong at worst, remove them to avoid confusion
+  }
+
+  return {
+    sequence: results,
+    missingTasks: missing,
+    recursiveDependencies: recursive
+  };
+};
--- test.js
+++ test.js
@@ -1,1 +1,133 @@
-2;
+/*global describe:false, it:false */
+
+'use strict';
+
+var sequencify = require('./');
+var should = require('chai').should();
+require('mocha');
+
+describe('task sequencing', function() {
+  describe('sequencify()', function() {
+
+    var dependencyTree = {
+      a: [],
+      b: ['a'],
+      c: ['a'],
+      d: ['b','c'],
+      e: ['f'],
+      f: ['e'],
+      g: ['g']
+    };
+    var noop = function () {};
+
+    var makeTasks = function (tree) {
+      var tasks = {}, p;
+      for (p in tree) {
+        if (tree.hasOwnProperty(p)) {
+          tasks[p] = {
+            name: p,
+            dep: tree[p],
+            fn: noop
+          };
+        }
+      }
+      return tasks;
+    };
+
+    var theTest = function (source,expected) {
+      // arrange
+      var tasks = makeTasks(dependencyTree);
+
+      // act
+      var actual = sequencify(tasks, source.split(','));
+
+      // assert
+      actual.sequence.join(',').should.equal(expected);
+      actual.missingTasks.length.should.equal(0);
+      actual.recursiveDependencies.length.should.equal(0);
+    };
+
+    var theTestError = function (source) {
+      // arrange
+      var tasks = makeTasks(dependencyTree);
+
+      // act
+      var actual = sequencify(tasks, source.split(','));
+
+      // assert done by caller
+      return actual;
+    };
+
+    it('a -> a', function() {
+      theTest('a', 'a');
+    });
+    it('a,a -> a', function() {
+      theTest('a,a', 'a');
+    });
+    it('c -> a,c', function() {
+      theTest('c', 'a,c');
+    });
+    it('b -> a,b', function() {
+      theTest('b', 'a,b');
+    });
+    it('c,b -> a,c,b', function() {
+      theTest('c,b', 'a,c,b');
+    });
+    it('b,c -> a,b,c', function() {
+      theTest('b,c', 'a,b,c');
+    });
+    it('b,a -> a,b', function() {
+      theTest('b,a', 'a,b');
+    });
+    it('d -> a,b,c,d', function() {
+      theTest('d', 'a,b,c,d');
+    });
+    it('c,d -> a,c,b,d', function() {
+      theTest('c,d', 'a,c,b,d');
+    });
+    it('b,d -> a,b,c,d', function() {
+      theTest('b,d', 'a,b,c,d');
+    });
+    it('e -> recursive', function() {
+      // arrange
+      var i;
+      var expectedRecursionList = ['e','f','e'];
+
+      // act
+      var actual = theTestError('e');
+
+      // assert
+      actual.recursiveDependencies.length.should.equal(1);
+      actual.recursiveDependencies[0].length.should.equal(expectedRecursionList.length);
+      for (i = 0; i < expectedRecursionList.length; i++) {
+        actual.recursiveDependencies[0][i].should.equal(expectedRecursionList[i]);
+      }
+    });
+    it('g -> recursive', function() {
+      // arrange
+      var i;
+      var expectedRecursionList = ['g','g'];
+
+      // act
+      var actual = theTestError('g');
+
+      // assert
+      actual.recursiveDependencies.length.should.equal(1);
+      actual.recursiveDependencies[0].length.should.equal(expectedRecursionList.length);
+      for (i = 0; i < expectedRecursionList.length; i++) {
+        actual.recursiveDependencies[0][i].should.equal(expectedRecursionList[i]);
+      }
+    });
+    it('h -> missing', function() {
+      // arrange
+
+      // act
+      var actual = theTestError('h');
+
+      // assert
+      actual.missingTasks.length.should.equal(1);
+      actual.missingTasks[0].should.equal('h');
+    });
+
+  });
+});
